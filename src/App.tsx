c20337f875a211e84d5bb699861895e006a4cf3a
Sebastián Suárez <serbastian.suarezr@udea.edu.co>
2023-08-29 04:44:19 +0000

    add a customizable background

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { type ShadowProps } from './types';
 import { ShadowForm } from './components/ShadowForm';
 import './App.css';
 import { hexToRbga } from './utils/color';
+import { Board } from './components/Board';
 
 export default function App() {
   const [shadow, setShadow] = useState<ShadowProps>({
@@ -15,48 +16,56 @@ export default function App() {
   });
 
   return (
-    <div className=''>
-      <div className='flex h-[100vh] border divide-x rounded-xl border-slate-700 divide-slate-700'>
-        <ShadowForm onChange={(s) => { setShadow(s); }} />
-        <div className='flex flex-col grow'>
-          <div className='flex items-center justify-center bg-white grow'>
-            <div
-              className='bg-gray-400 h-36 rounded-2xl w-36 '
-              style={
-              {
-                boxShadow:
-                `${shadow.inset ? 'inset' : ''} 
+    <div
+      className='flex h-[100vh] border divide-x rounded-xl
+      border-slate-700 divide-slate-700 flex-wrap'
+    >
+      <aside className='grow shrink-0'>
+        <ShadowForm
+          onChange={(s) => {
+            setShadow(s);
+          }}
+        />
+      </aside>
+      <main className='flex flex-col grow basis-96'>
+        <Board
+          backgroundImagesUrl={['src/assets/grid.svg', 'src/assets/dots.svg']}
+        >
+          <div
+            className='w-36 h-36 bg-slate-500 rounded-2xl'
+            style={{
+              boxShadow: `${shadow.inset ? 'inset' : ''} 
                 ${shadow.offsetX}px 
                 ${shadow.offsetY}px 
                 ${shadow.blurRadius}px 
                 ${shadow.spreadRadius}px 
                 ${shadow.color}`,
-              }
-            }
-            />
-          </div>
-          <div className='p-4'>
-            <p>
-              Tailwindcss classes:
-              <br />
-              <code className='bg-gray-950'>
-                {
-                  `shadow-[${shadow.inset ? 'inset_' : ''}${shadow.offsetX}px_${shadow.offsetY}px_${shadow.blurRadius}px_${shadow.spreadRadius}px_${shadow.color}]`
-                }
-              </code>
-            </p>
-            <p>
-              CSS code:
-              <br />
-              <code className='bg-gray-950'>
-                {`box-shadow: ${shadow.inset ? 'inset' : ''} 
+            }}
+          />
+        </Board>
+        <footer className='p-4'>
+          <p>
+            Tailwindcss classes:
+            <br />
+            <code className='p-1 rounded-sm w-15 bg-gray-950'>
+              {`shadow-[${shadow.inset ? 'inset_' : ''}${shadow.offsetX}px_${
+                shadow.offsetY
+              }px_${shadow.blurRadius}px_${shadow.spreadRadius}px_${
+                shadow.color
+              }]`}
+            </code>
+          </p>
+          <p>
+            CSS code:
+            <br />
+            <code className='p-1 rounded-sm w-15 bg-gray-950'>
+              {`box-shadow: ${shadow.inset ? 'inset' : ''} 
               ${shadow.offsetX}px ${shadow.offsetY}px ${shadow.blurRadius}px 
               ${shadow.spreadRadius}px ${shadow.color}`}
-              </code>
-            </p>
-          </div>
-        </div>
-      </div>
+            </code>
+          </p>
+        </footer>
+      </main>
     </div>
   );
 }
---
+++ src/components/Board.tsx
@@ -0,0 +1,63 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+// eslint-disable jsx-a11y/click-events-have-key-events
+// eslint-disable jsx-a11y/click-events-have-key-events
+// eslint-disable jsx-a11y/no-static-element-interactions
+import { ReactNode, useState } from 'react';
+import { Toggle } from './Toggle';
+
+type Props = {
+  children: ReactNode;
+  backgroundImagesUrl: string[];
+};
+
+export function Board({ children, backgroundImagesUrl }: Props) {
+  const [dark, setDark] = useState(false);
+  const [bgImageIndex, setBgImageIndex] = useState<number>();
+  const bgImages = backgroundImagesUrl;
+  return (
+    <div
+      className='flex relative justify-center items-center min-h-[24rem] grow bg-gray-950'
+      style={{
+        backgroundImage:
+          bgImageIndex || bgImageIndex === 0
+            ? `url(${bgImages[bgImageIndex]})`
+            : 'none',
+      }}
+    >
+      <div className='z-20'>{children}</div>
+      <div
+        className='absolute top-0 left-0 z-10 w-full h-full'
+        style={{
+          backdropFilter: dark ? 'none' : 'invert(0.9)',
+          WebkitTransition: 'backdrop-filter 500ms ease-in-out',
+        }}
+      />
+      <div className='absolute flex gap-3 right-1 bottom-1'>
+        <Toggle
+          className='z-20'
+          onChange={(checked) => {
+            setDark(checked);
+          }}
+        />
+        <div className='z-20 flex gap-3'>
+          {bgImages.map((imgUrl, index) => (
+            <div
+              className='w-16 h-16 rounded-lg bg-gray-950 bg-[length:300px_300px]
+            border border-gray-200'
+              style={{
+                backgroundImage: `url(${imgUrl})`,
+              }}
+              onClick={() => setBgImageIndex(index)}
+            />
+          ))}
+          <div
+            className='w-16 h-16 rounded-lg bg-gray-950 bg-[length:300px_300px]
+            border border-gray-200'
+            onClick={() => setBgImageIndex(undefined)}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
